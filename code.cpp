d1fc5a6a8c20f65d882598275885df1379a75e89
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2016-01-30 14:39:21 -0600

    Add variables and new output to the code.cpp file.

--- code.cpp
+++ code.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-	
-	
+	int a = 10, b = 50, c;
+	c = a + b;
+
 	cout<<" This  is my practice for git pushing and commits"<<endl;
-	
-	
+	cout << "This is a new addition" << endl;	
+	cout << c << endl;
 	
 	
 	
